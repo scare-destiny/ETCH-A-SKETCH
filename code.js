c0051c7051de5fe819feb6245af55e28946032f8
scare-destiny <zhenya.venger@gmail.com>
2022-04-10 17:30:17 +0300

    Added beforeunload event

--- code.js
+++ code.js
@@ -115,3 +115,10 @@ function askUser() {
 	console.log(answer, answer);
 	makeGrid(answer, answer);
 }
+
+window.addEventListener('beforeunload', function (e) {
+	// Cancel the event
+	e.preventDefault(); // If you prevent default behavior in Mozilla Firefox prompt will always be shown
+	// Chrome requires returnValue to be set
+	e.returnValue = '';
+  });
